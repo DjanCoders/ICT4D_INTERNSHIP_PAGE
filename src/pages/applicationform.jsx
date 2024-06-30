6fc647294f0f49a24b24030ef580d94bc1ca9cb8
jenberu <jemberu0970@gmail.com>
2024-06-30 11:02:28 +0300

    updated

--- src/pages/applicationform.jsx
+++ src/pages/applicationform.jsx
@@ -44,105 +44,121 @@ const ApplicationForm = () => {
 
   return (
     <div className="App">
-    <h1>Internship Application Form</h1>
+      <h1>Internship Application </h1>
+      <h2>Please complete the form below to apply for Internship with us.</h2>
 
     <form onSubmit={handleSubmit}>
-      <div>
-        <label className='block text-sm font-medium text-gray-1000'> First Name:</label>
+        <div className='full-name'>
+          <span> Full Name</span>
+          <br/>
+        <label className='text-sm font-medium text-gray-1000'> First Name:</label>
           <input type="text" name="fname" value={formData.fname} onChange={handleChange} required
-          className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+          className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
+          />
+       
+         <label className=' text-sm font-medium text-gray-1000'> Last Name:</label>
+          <input type="text" name="lname" value={formData.lname} onChange={handleChange} required
+        className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
+          />
+      </div>
+        <div className='contact-address'>
+        <span> Contact Address</span>
+        <br/>
+        <label className='text-sm font-medium text-gray-1000'>Email:</label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required
+                   className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
+
+          />
+      
+        <label className='text-sm font-medium text-gray-1000'>Phone:</label>
+        <input type="tel" name="phone" value={formData.phone} onChange={handleChange} 
+                  className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
+           />
+      </div>
+      <div className='address'>
+        <label className='text-sm font-medium text-gray-1000' >Address:</label>
+          <input type="text" name="address" value={formData.address} onChange={handleChange}
+                 className='appearance-none adress-input  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
 
           />
         </div>
-        <div>
-        <label className='block text-sm font-medium text-gray-1000'> Last Name:</label>
-          <input type="text" name="lname" value={formData.lname} onChange={handleChange} required
-          className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+        <div className='school-info'> 
+          <span> School Information</span>
 
-          />
-      </div>
       <div>
-        <label className='block text-sm font-medium text-gray-1000'>Email:</label>
-          <input type="email" name="email" value={formData.email} onChange={handleChange} required
-          className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
-
-          />
-      </div>
-      <div>
-        <label className='block text-sm font-medium text-gray-1000'>Phone:</label>
-        <input type="tel" name="phone" value={formData.phone} onChange={handleChange} 
-         className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
-           />
-      </div>
-      <div>
-        <label className='block text-sm font-medium text-gray-1000' >Address:</label>
-          <input type="text" name="address" value={formData.address} onChange={handleChange}
-        className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
-
-          />
-      </div>
-      <div>
-        <label className='block text-sm font-medium text-gray-1000' >School/University:</label>
+        <label className='text-sm font-medium text-gray-1000' >School/University:</label>
         <input type="text" name="school" value={formData.school} onChange={handleChange} 
-         className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+                  className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
            />
-      </div>
-      <div>
-        <label className='block text-sm font-medium text-gray-1000'>Degree:</label>
+      
+        <label className='text-sm font-medium text-gray-1000'>Degree:</label>
         <input type="text" name="degree" value={formData.degree} onChange={handleChange} 
-         className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+                  className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
            />
       </div>
       <div>
-        <label className='block text-sm font-medium text-gray-1000'>Major:</label>
+        <label className='text-sm font-medium text-gray-1000'>Major:</label>
         <input type="text" name="major" value={formData.major} onChange={handleChange} 
-         className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+                  className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
            />
-      </div>
-      <div>
-        <label className='block text-sm font-medium text-gray-1000'>CGPA:</label>
+      
+        <label className='text-sm font-medium text-gray-1000'>CGPA:</label>
           <input type="text" name="gpa" value={formData.gpa} onChange={handleChange}
-        className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+                 className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
 
           />
       </div>
-     
-      <div>
-        <label className='block text-sm font-medium text-gray-1000'>Start Date:</label>
+      </div>
+        <div className='date-info'>
+          <span>Date Information</span>
+          <br />
+          
+        <label className='text-sm font-medium text-gray-1000'>Start Date:</label>
         <input type="date" name="startDate" value={formData.startDate} onChange={handleChange}  
-         className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+                  className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
      
           />
-      </div>
-      <div>
-        <label className='block text-sm font-medium text-gray-1000'>Duration (months):</label>
+      
+        <label className='text-sm font-medium text-gray-1000'>Duration (months):</label>
         <input type="number" name="duration" value={formData.duration} onChange={handleChange}  
-         className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+        className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
      
           />
       </div>
       <div>
-        <label className='block text-sm font-medium text-gray-1000'>Department/Role Interested In:</label>
+        <label className='text-sm font-medium text-gray-1000'>Department/Role Interested In:</label>
         <input type="text" name="department" value={formData.department} onChange={handleChange}  
-         className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+          className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+
      
           />
       </div>
       <div>
-        <label className='block text-sm font-medium text-gray-1000'>Resume:</label>
+        <label className='text-sm font-medium text-gray-1000'>Resume:</label>
         <input type="file" name="resume" onChange={handleChange}  
-         className='appearance-none block w-full px-3 py-2 border border-gray-500 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+          className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
      
           />
       </div>
       <div>
-        <label className='block text-sm font-medium text-gray-1000'>Cover Letter:</label>
+        <label className='text-sm font-medium text-gray-1000'>Cover Letter:</label>
           <input type="file" name="coverLetter" onChange={handleChange}
-        className='appearance-none block w-400px px-3 py-2 border border-blue-500 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+          className='appearance-none  px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
 
            />
       </div>
-      <button type="submit">Submit</button>
+      <button className='form-button' type="submit">Submit</button>
     </form>
     </div>
   );
