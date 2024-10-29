7f7dbd5d278758b023b65f627abf40d67f6b5420
Elianamos <elianamos29@gmail.com>
2024-10-29 18:24:59 +0300

    update service card

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,10 +3,22 @@ import ServiceCard from "./ServiceCard";
 import { getInternships } from "../api";
 
 const Services = () => {
-	const internService = internships.map((service) => (
-		<ServiceCard service={service} />
-	));
-	return <div className="flex flex-col md:flex-row gap-8">{internService}</div>;
+  const [internships, setInternships] = useState<{ title: string; description: string; }[]>([]);
+
+  useEffect(() => {
+    const fetchInternships = async () => {
+      const response = await getInternships();
+      setInternships(response.data);
+    };
+
+    fetchInternships();
+  }, []);
+
+  const internService = internships.map((internship, index) => (
+    <ServiceCard key={index} service={internship} />
+  ));
+
+  return <div className='flex flex-col md:flex-row gap-8'>{internService}</div>;
 };
 
 export default Services;
