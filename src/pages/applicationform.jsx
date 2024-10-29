d074ed697074ddea953e778c10f6e1f8de843d3d
jenberu <jemberu0970@gmail.com>
2024-10-29 10:43:18 +0300

    updated

--- src/pages/applicationform.jsx
+++ src/pages/applicationform.jsx
@@ -7,6 +7,7 @@ import coderLogo from '../components/Images/coder-logo.jpg';
 const ApplicationForm = () => {
   const [submissionMessage, setSubmissinMessage] = useState(" ");
   const [hasError, setHasError] = useState(false);
+  
   const [formData, setFormData] = useState({
     first_name: '',
     last_name: '',
@@ -23,10 +24,10 @@ const ApplicationForm = () => {
     resume: null,
     cover_letter: null,
   });
-
+ 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
-    if (files) {
+    if (files && files.length > 0) {
       setFormData({
         ...formData,
         [name]: files[0],
@@ -41,6 +42,11 @@ const ApplicationForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const data = new FormData();
+    for (const [key, value] of Object.entries(formData)) {
+      data.append(key, value);
+    }
    
   
     try {
@@ -83,7 +89,7 @@ const ApplicationForm = () => {
     <h3 className="font-bold text-lg mt-4">Key Dates</h3>
     <ul>
       <li><strong>Application Start:</strong> {formData.start_date}</li>
-      <li><strong>Application Deadline:</strong> [Date]</li>
+      <li><strong>Application Deadline:</strong> </li>
     </ul>
 
     <h3 className="font-bold text-lg mt-4">FAQs</h3>
