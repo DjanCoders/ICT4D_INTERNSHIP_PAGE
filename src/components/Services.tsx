244c86a1c30535a9d647aeea17352d21d836bb15
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-10-29 21:39:26 +0300

    ui-fix

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -5,7 +5,11 @@ const Services = () => {
 	const internService = internships.map((service) => (
 		<ServiceCard service={service} />
 	));
-	return <div className="flex flex-col md:flex-row gap-8">{internService}</div>;
+	return (
+		<div className="flex flex-col md:grid grid-cols-3 flex-wrap gap-8">
+			{internService}
+		</div>
+	);
 };
 
 export default Services;
